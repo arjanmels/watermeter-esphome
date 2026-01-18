087bf248cc489d3989c4574a859cf481d35d7669
martijn <martijnvanwezel@experiencedata.nl>
2026-01-18 15:45:07 +0100

    fix production installer always get latest esphome

--- production_installer.sh
+++ production_installer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
-docker run --rm --privileged -v  ${PWD}:/config   -it ghcr.io/esphome/esphome compile  "muino-water-meter-esp32.yaml"
+docker pull ghcr.io/esphome/esphome:latest
+docker run --rm --privileged -v  ${PWD}:/config   -it ghcr.io/esphome/esphome compile  "muino-water-meter-esp32.factory.yaml"
 counter=1
 while true; do
     if [ -e "/dev/ttyACM0" ]; then
         counter=$((counter+1))
         echo -e "\033[34mGoing to program $counter times\033[0m"
-        # ...existing code...
-        docker run --rm --privileged -v  ${PWD}:/config   -it ghcr.io/esphome/esphome upload  --device=/dev/ttyACM0 "muino-water-meter-esp32.yaml"
-        docker run --rm --privileged -v  ${PWD}:/config   -it ghcr.io/esphome/esphome logs  --device=/dev/ttyACM0 "muino-water-meter-esp32.yaml"
+
+        docker run --rm --privileged -v  ${PWD}:/config   -it ghcr.io/esphome/esphome upload  --device=/dev/ttyACM0 "muino-water-meter-esp32.factory.yaml"
+        docker run --rm --privileged -v  ${PWD}:/config   -it ghcr.io/esphome/esphome logs  --device=/dev/ttyACM0 "muino-water-meter-esp32.factory.yaml"
         echo -e "\033[34mEntered the program $counter times\033[0m"
 
     fi
